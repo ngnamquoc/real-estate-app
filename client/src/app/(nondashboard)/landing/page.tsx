8864826ba38292b73c817a790106b814adaea95c
ngnamquoc <nguyennamquoc.mar@gmail.com>
2025-05-06 20:45:44 -0400

    implemented feature section UI

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import FeatureSection from './FeatureSection'
 
 const landing = () => {
   return (
     <div>
       {/* Landing page */}
       <HeroSection/>
+      <FeatureSection/>
     </div>
   )
 }
